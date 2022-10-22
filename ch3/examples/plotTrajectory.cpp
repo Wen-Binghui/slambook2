5753b11f38520ee76d3b97aeef75536c98f24c4e
Wen-Binghui <wbhwenbinghui@gmail.com>
2022-10-23 01:40:00 +0200

    1

--- ch3/examples/plotTrajectory.cpp
+++ ch3/examples/plotTrajectory.cpp
@@ -1,18 +1,22 @@
 #include <pangolin/pangolin.h>
 #include <Eigen/Core>
 #include <unistd.h>
-
 // 本例演示了如何画出一个预先存储的轨迹
 
 using namespace std;
 using namespace Eigen;
 
 // path to trajectory file
-string trajectory_file = "./examples/trajectory.txt";
+string trajectory_file = "trajectory.txt";
 
 void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>>);
 
 int main(int argc, char **argv) {
+
+  char tmp[256];
+  getcwd(tmp, 256);
+  cout << "Current working directory: " << tmp << endl;
+  
 
   vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>> poses;
   ifstream fin(trajectory_file);
